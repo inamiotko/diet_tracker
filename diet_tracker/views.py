fc2a4770890b6e18e51eecacc2a43b984121fa57
inamiotko <iganamiotko14@gmail.com>
2020-08-20 10:58:20 +0200

    Added  html basic template

--- diet_tracker/views.py
+++ diet_tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Meal
 # Create your views here.
+def meals_list(request):
+    meals=[]
+    meals = Meal.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request,'diet_tracker/meals_list.html', {'meals':meals})
