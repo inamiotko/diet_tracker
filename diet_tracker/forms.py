1deb9b5f2befa56cb9658fa72dd1bcc4991008de
inamiotko <iganamiotko14@gmail.com>
2020-08-24 14:19:10 +0200

    added button that redirects to adding new meals form

--- diet_tracker/forms.py
+++ diet_tracker/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from .models import Meal
 from django.utils import timezone
 class UpdateMeal(forms.ModelForm):
-    
     class Meta:
         model = Meal
-       
         fields = ('meal', 'calories', 'description')
+        
